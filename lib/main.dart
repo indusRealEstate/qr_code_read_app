12bd99b9787d94fe278c45a37edaec9d15241356
indusRealEstate <webtech@indusre.ae>
2024-03-20 11:01:48 +0400

    refactor: :zap: removed hive deps.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:qr_code_reader_app/bindings/base_bindings.dart';
 import 'package:qr_code_reader_app/routes/app_routes.dart';
 import 'package:qr_code_reader_app/routes/getx_app_pages.dart';
 
 Future<void> main() async {
-  await Hive.initFlutter();
   runApp(const MyApp());
 }
 
